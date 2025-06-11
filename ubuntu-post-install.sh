0c0dec94711b59907f6d2580b42694de55545ff1
willrocknroll <62892595+willrocknroll@users.noreply.github.com>
2025-06-11 15:41:30 -0300

    Adicionados programas para instalação em Flatpak

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -11,11 +11,9 @@
 # Atualizando o sistema #
 #
 echo "Atualizando o sistema..."
-
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get autoclean -y
-sudo apt-get autoremove -y
+
 
 #
 # Instalando programas
@@ -31,9 +29,6 @@ sudo add-apt-repository universe
 sudo apt-get update
 sudo apt-get install putty -y
 
-echo "Instalando Google Chrome"
-sudo apt-get install google-chrome-stable -y
-
 echo "Instalando Virt-manager"
 sudo apt-get install ssh-askpass
 sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
@@ -45,23 +40,34 @@ sudo systemctl enable libvirtd
 sudo apt install virt-manager -y
 sudo systemctl status libvirtd
 
-
 echo "Instalando Unzip"
 sudo apt-get install unzip -y
 
 echo "Instalando Zip"
 sudo apt-get install zip -y
 
-echo "Instalando VS Code"
-sudo apt update
-sudo apt install -y curl apt-transport-https
-curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo gpg --dearmor -o /usr/share/keyrings/ms-vscode-keyring.gpg
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/ms-vscode-keyring.gpg] https://packages.microsoft.com/repos/vscode stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
-sudo apt update
-sudo apt install -y code
-sudo snap refresh
-sudo snap install code --classic
+echo "Instalando Ajustes do Gnome"
+sudo apt install gnome-tweak-tool -y
 
+echo "Instalando Git"
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt update && apt install git -y
+
+echo "Instalando programas em FlatPak"
+flatpak install flathub com.mattjakeman.ExtensionManager -y
+flatpak install flathub com.google.Chrome -y
+flatpak install flathub com.microsoft.Edge -y
+flatpak install flathub com.github.IsmaelMartinez.teams_for_linux -y
+flatpak install flathub org.qbittorrent.qBittorrent -y
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub org.videolan.VLC -y
+flatpak install flathub io.github.shiftey.Desktop -y
+flatpak install flathub io.github.thetumultuousunicornofdarkness.cpu-x -y
+flatpak install flathub org.angryip.ipscan -y
+flatpak install flathub md.obsidian.Obsidian -y
+flatpak install flathub org.keepassxc.KeePassXC -y
+flatpak install flathub io.github.vikdevelop.SaveDesktop -y
+flatpak install flathub com.obsproject.Studio -y
 
 #
 # Finalizando instalações
